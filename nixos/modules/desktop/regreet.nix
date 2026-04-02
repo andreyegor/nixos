be18855321a7b3ce622fe6de823e820cc7ade416
Egor <andreevegor10000@gmail.com>
2026-04-03 02:47:01 +0700

    я устал придумывать названия коммитов

--- nixos/modules/desktop/regreet.nix
+++ nixos/modules/desktop/regreet.nix
@@ -5,18 +5,17 @@
   ...
 }:
 {
-  programs.regreet.enable = true;
-  programs.regreet.settings = {
-    background = {
-      path = "/home/egor/Pictures/Wallpapers/lonely-fish.png";
-      fit = "Cover";
-    };
-    GTK = {
-      cursor_theme_name = "Adwaita";
-      font_name = "Cantarell 12";
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri";
+        user = "egor";
+      };
+      default_session = {
+        command = "${pkgs.greetd.gtkgreet}/bin/gtkgreet -c niri";
+        user = "greeter";
+      };
     };
   };
-
-  services.greetd.settings.default_session.command =
-    "${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri -c /etc/greetd/niri-config.kdl";
 }
