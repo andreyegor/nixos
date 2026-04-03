4dbdc5aaa8e6102d1bb541111cdfa9913ba0a5e2
Egor <andreevegor10000@gmail.com>
2026-04-03 12:41:59 +0700

    add Throne and smth else

--- nixos/modules/system/environment.nix
+++ nixos/modules/system/environment.nix
@@ -8,7 +8,9 @@
     MOZ_ENABLE_WAYLAND = 1; # For firefox
     WEBKIT_DISABLE_DMABUF_RENDERER = 1; # For hoppscotch
     QT_QPA_PLATFORM = "wayland;xcb";
-    GTK_THEME = "Adwaita-dark";
+    GTK_THEME = "Adwaita:dark";
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+    CHROMIUM_FLAGS = "--enable-features=UseOzonePlatform --ozone-platform=wayland --force-dark-mode";
     XCURSOR_SIZE = "24";
   };
   environment.etc = {
