21bfbc4a65dff1d900d60d84264547bb8217160e
Egor <andreevegor10000@gmail.com>
2026-04-03 14:16:28 +0700

    autorun throne

--- nixos/modules/desktop/greetd.nix
+++ nixos/modules/desktop/greetd.nix
@@ -8,14 +8,15 @@
   services.greetd = {
     enable = true;
     settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --autologin --cmd '${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri'";
+        user = "greeter";
+      };
       initial_session = {
         command = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.niri}/bin/niri";
         user = "egor";
       };
-      default_session = {
-        command = "${pkgs.greetd.gtkgreet}/bin/gtkgreet -c niri";
-        user = "greeter";
-      };
     };
   };
+  security.pam.services.greetd.enableGnomeKeyring = true;
 }
