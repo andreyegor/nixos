79014e87bbc32ba6ac8e98573a66cb0dfacc0a1b
Egor <andreevegor10000@gmail.com>
2026-04-06 13:00:03 +0700

    upd ollama

--- nixos/hosts/laptop-ga401qm/programs.nix
+++ nixos/hosts/laptop-ga401qm/programs.nix
@@ -1,16 +1,20 @@
 {
   config,
   pkgs,
+  pkgsUnstable,
   ...
 }:
 {
   services.ollama = {
     enable = true;
+    package = pkgsUnstable.ollama;
     # Optional: preload models, see https://ollama.com/library
     loadModels = [
+      "qwen3:4b"
+      "qwen3.5:4b"
+      "nemotron-3-nano:4b"
       "qwen2.5-coder:7b"
       "deepseek-r1:8b"
-      "glm-4.7-flash"
     ];
   };
 }
