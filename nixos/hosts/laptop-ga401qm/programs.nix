39e91afb69bf622ce8d13860f22afabc0a8ee1a6
Egor <andreevegor10000@gmail.com>
2026-04-09 13:11:29 +0700

    fix ollama

--- nixos/hosts/laptop-ga401qm/programs.nix
+++ nixos/hosts/laptop-ga401qm/programs.nix
@@ -7,10 +7,10 @@
 {
   services.ollama = {
     enable = true;
-    package = pkgsUnstable.ollama;
+    package = pkgsUnstable.ollama-cuda;
     # Optional: preload models, see https://ollama.com/library
     loadModels = [
-      "nemotron-3-nano:4b" # Длям агентов
+      "nemotron-3-nano:4b" # Для агентов
       "qwen2.5-coder:7b" # Самый умный
       "qwen3.5:4b" # Reasoninig
       "gemma4:e4b" # Самый быстрый
--- nixos/modules/desktop/nvidia.nix
+++ nixos/modules/desktop/nvidia.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 
@@ -12,4 +13,15 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+  nixpkgs.config.nvidia.acceptLicense = true;
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "cuda_cccl"
+      "cuda_cudart"
+      "cuda_nvcc"
+      "libcublas"
+      "nvidia-settings"
+      "nvidia-x11"
+    ];
 }
