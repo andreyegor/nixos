c65b08c2a387b1ce8b8a6c2ef40a1944aef1786f
Egor <andreevegor10000@gmail.com>
2026-05-07 12:34:38 +0700

    pururum

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -1,11 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./modules/appearance/cursor.nix
-  ];
+
   home.username = "egor";
   home.homeDirectory = "/home/egor";
+
+  home.pointerCursor = {
+    name = "catppuccin-mocha-dark-cursors";
+    package = pkgs.catppuccin-cursors.mochaDark;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
+  home.sessionVariables = {
+    XCURSOR_THEME = "catppuccin-mocha-dark-cursors";
+    XCURSOR_SIZE = "24";
+    QT_QPA_PLATFORMTHEME = "gtk3";
+  };
 
   home.stateVersion = "25.11";
 }
