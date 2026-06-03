d484c1ff4b71e70e51afec50f65ede53785e5e50
Egor <andreevegor10000@gmail.com>
2026-06-04 00:48:33 +0700

    upd ollama

--- home/modules/packages/programs/opencode.nix
+++ home/modules/packages/programs/opencode.nix
@@ -9,10 +9,7 @@
           name = "Ollama";
           options.baseURL = "http://127.0.0.1:11434/v1";
           models = {
-            "nemotron-3-nano:4b".name = "Nemotron 3 nano 4b";
-            "llama3.1:8b".name = "Llama 3.1 8b";
-            "qwen2.5-coder:7b".name = "Qwen2.5 coder 7b";
-            "qwen3.5:4b".name = "Qwen 3.5 4b";
+            "lfm2.5".name = "Lfm 2.5 8b";
           };
         };
       };
--- hosts/laptop-ga401qm/programs.nix
+++ hosts/laptop-ga401qm/programs.nix
@@ -1,16 +1,13 @@
 {
-  pkgs,
+  pkgsUnstable,
   ...
 }:
 {
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-cuda;
+    package = pkgsUnstable.ollama-cuda;
     loadModels = [
-      "nemotron-3-nano:4b"
-      "llama3.1:8b"
-      "qwen2.5-coder:7b"
-      "qwen3.5:4b"
+      "lfm2.5"
     ];
   };
 }
