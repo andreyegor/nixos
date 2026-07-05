8bf0565788fb9fc68e258034fa179709b8d4ff8a
Egor <andreevegor@gmail.com>
2026-07-06 02:12:23 +0700

    pc suspend fix

--- hosts/altai-pc/device.nix
+++ hosts/altai-pc/device.nix
@@ -3,4 +3,16 @@
   services.xserver.videoDrivers = [
     "nvidia"
   ];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
+
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_EnableGpuFirmware=0
+    options nvidia NVreg_PreserveVideoMemoryAllocations=1
+    options nvidia NVreg_TemporaryFilePath=/var/tmp
+  '';
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+  ];
 }
